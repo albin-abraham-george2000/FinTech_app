17b2ea4af2b5744e2af6e3d5c65937b6b1cde413
Albin Abraham George <167897216+albin-abraham-george2000@users.noreply.github.com>
2024-06-07 11:56:17 +0530

    Update settings.py

--- FinTech_app/settings.py
+++ FinTech_app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*","127.0.0.1","fintech-pk92.onrender.com"]
 
 
 # Application definition
